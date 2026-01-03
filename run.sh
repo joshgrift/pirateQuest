0c61630b60878505855c431b0729b4d0fc2b611f
joshgrift <josh@grift.ca>
2026-01-03 12:19:25 -0800

    run.sh --server option

--- run.sh
+++ run.sh
@@ -12,17 +12,23 @@ echo -e "${RED}=== Starting Server ===${RESET}"
 /Applications/Godot_mono.app/Contents/MacOS/Godot --path . --position 0,50 --server 2>&1 | sed "s/^/$(echo -e ${RED})[Server]$(echo -e ${RESET}) /" &
 PID1=$!
 
-sleep 0.5
+# Only start clients if --server flag is not present
+if [[ "$1" != "--server" ]]; then
+  sleep 0.5
 
-echo -e "${GREEN}=== Starting Client 1 ===${RESET}"
-/Applications/Godot_mono.app/Contents/MacOS/Godot --path . --resolution 2400x1000 --position 0,50 --client1 2>&1 | sed "s/^/$(echo -e ${GREEN})[Client 1 ]$(echo -e ${RESET}) /" &
-PID2=$!
+  echo -e "${GREEN}=== Starting Client 1 ===${RESET}"
+  /Applications/Godot_mono.app/Contents/MacOS/Godot --path . --resolution 2400x1000 --position 0,50 --client1 2>&1 | sed "s/^/$(echo -e ${GREEN})[Client 1 ]$(echo -e ${RESET}) /" &
+  PID2=$!
 
-echo -e "${BLUE}=== Starting Client 2 ===${RESET}"
-/Applications/Godot_mono.app/Contents/MacOS/Godot --path . --resolution 2400x1000 --position 0,1250 --client2 2>&1 | sed "s/^/$(echo -e ${BLUE})[Client 2]$(echo -e ${RESET}) /" &
-PID3=$!
+  echo -e "${BLUE}=== Starting Client 2 ===${RESET}"
+  /Applications/Godot_mono.app/Contents/MacOS/Godot --path . --resolution 2400x1000 --position 0,1250 --client2 2>&1 | sed "s/^/$(echo -e ${BLUE})[Client 2]$(echo -e ${RESET}) /" &
+  PID3=$!
 
-# Wait for both instances to complete
-wait $PID1 $PID2 $PID3
+  # Wait for all instances to complete
+  wait $PID1 $PID2 $PID3
+else
+  # Wait for server only
+  wait $PID1
+fi
 
 echo "=== Terminated ==="
